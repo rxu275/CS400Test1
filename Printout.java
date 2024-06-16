fe9b2bbd66c9e6bfebfda5d965f44da226267b98
rxu275 <rxu275@wisc.edu>
2024-06-16 00:10:29 -0500

    New changes to print out

--- Printout.java
+++ Printout.java
@@ -1,6 +1,6 @@
 // A test file for printing out a sentence.
 Public class Printout {
     public static void main(String[] args){
-        System.out.println("Just a test.");
+        System.out.println("Just a test and more!");
     }
 }
